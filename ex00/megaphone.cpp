20fa1904b168ba971d3e14e863c4df3ab635f149
rnakaa <102458195+rnakaa@users.noreply.github.com>
2023-11-18 22:43:00 +0900

    <feat> : <new func ex00>
    Commit type: feat
    
    Scope: (A brief description of the part of the codebase affected by the commit)
    
    Long description (wrap at 72 characters):
    
    Issue or Pull Request: (e.g., "fixes: #123" or "closes: #456" or "Issue # 789", "PR #1000")
    
     Issue: #42

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rnaka <rnaka@student.42tokyo.jp>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/17 10:40:08 by rnaka             #+#    #+#             */
+/*   Updated: 2023/11/18 22:42:29 by rnaka            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+
+// ヘッダ <cctype> で定義
+// 		
+// int toupper( int ch );
+// char を　intにキャストした上でtoupper
+
+int	main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	for (size_t i = 1; i < argc; i++)
+	{
+		for (size_t j = 0; j < std::strlen(argv[i]); j++)
+		{
+			std::cout << static_cast<char>(std::toupper(argv[i][j]));
+		}
+		if (i != argc - 1)
+			std::cout << ' ';
+	}
+	std::cout << std::endl;
+	return (0);
+}
