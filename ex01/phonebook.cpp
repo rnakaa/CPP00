91364d43414f1522032e15f206de3e17685dd2fe
rnakaa <102458195+rnakaa@users.noreply.github.com>
2023-11-21 23:58:43 +0900

    :sparkles:<feat>(main) : add exit
    Commit type:
    
    Description :
    
    Motivation:

--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -1,11 +1,25 @@
-class PhoneBook{
-private:
-	int number;
-	std::string name;
-public:
+class PhoneBook
+{
+	private:
+		int number;
+		std::string name;
+	public:
+};
+
+class Contact
+{
+	
 };
 
 int	main()
 {
+	string	command;
+
+	while (true)
+	{
+		std::cin >> command;
+		if (strcmp(command, "EXIT")
+			return (0);
+	}
 	return (0);
 }
